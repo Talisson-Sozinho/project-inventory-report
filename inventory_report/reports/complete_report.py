e5ac79b3163948ecc24859df5dde9a562640a471
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-05-01 00:06:44 -0300

    feito requisito 03 - implementado a classe "CompleteReport"

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,17 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, data: list[dict]) -> str:
+        simple_report = super().generate(data)
+        quantity_of_products_per_company = cls.get_products_per_company(data)
+        complete_report = ""
+        for company, quantity in quantity_of_products_per_company.items():
+            complete_report += f" - {company}: {quantity}\n"
+
+        return (
+            f"{simple_report}\n"
+            "Produtos estocados por empresa:\n"
+            f"{complete_report}"
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -8,10 +8,11 @@ class SimpleReport:
         earliest_expiration_date = cls.get_min_expiration_date(data)
         company_with_more_products = cls.get_company_with_more_products(data)
 
-        return f"""Data de fabricação mais antiga: {earliest_date_manufacture}
-Data de validade mais próxima: {earliest_expiration_date}
-Empresa com mais produtos: {company_with_more_products}
-"""
+        return (
+            f"Data de fabricação mais antiga: {earliest_date_manufacture}\n"
+            f"Data de validade mais próxima: {earliest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
 
     @staticmethod
     def get_min_date_manufacture(data: list[dict]) -> str:
@@ -31,12 +32,16 @@ Empresa com mais produtos: {company_with_more_products}
         )
 
     @staticmethod
-    def get_company_with_more_products(data: list[dict]) -> str:
+    def get_products_per_company(data: list[dict]) -> dict:
         count_company = {}
         for product in data:
             if product["nome_da_empresa"] in count_company:
                 count_company[product["nome_da_empresa"]] += 1
             else:
                 count_company[product["nome_da_empresa"]] = 1
+        return count_company
 
+    @classmethod
+    def get_company_with_more_products(cls, data: list[dict]) -> str:
+        count_company = cls.get_products_per_company(data)
         return max(count_company.items(), key=lambda company: company[1])[0]
