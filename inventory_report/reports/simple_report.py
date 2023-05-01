03917da85af1506511e89ebcc375b0237a7b781b
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-04-30 22:45:34 -0300

    feito requisito 02 - implementação da classe "SimpleReport"

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,42 @@
+from datetime import date
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, data: list[dict]) -> str:
+        earliest_date_manufacture = cls.get_min_date_manufacture(data)
+        earliest_expiration_date = cls.get_min_expiration_date(data)
+        company_with_more_products = cls.get_company_with_more_products(data)
+
+        return f"""Data de fabricação mais antiga: {earliest_date_manufacture}
+Data de validade mais próxima: {earliest_expiration_date}
+Empresa com mais produtos: {company_with_more_products}
+"""
+
+    @staticmethod
+    def get_min_date_manufacture(data: list[dict]) -> str:
+        return min(data, key=lambda data: data["data_de_fabricacao"])[
+            "data_de_fabricacao"
+        ]
+
+    @staticmethod
+    def get_min_expiration_date(data: list[dict]) -> str:
+        today = date.today()
+        return min(
+            [
+                product["data_de_validade"]
+                for product in data
+                if date.fromisoformat(product["data_de_validade"]) > today
+            ]
+        )
+
+    @staticmethod
+    def get_company_with_more_products(data: list[dict]) -> str:
+        count_company = {}
+        for product in data:
+            if product["nome_da_empresa"] in count_company:
+                count_company[product["nome_da_empresa"]] += 1
+            else:
+                count_company[product["nome_da_empresa"]] = 1
+
+        return max(count_company.items(), key=lambda company: company[1])[0]
